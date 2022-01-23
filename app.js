c2b860dc183bf576e6d7c411eb438f1544a29a5e
Hirenkunar <hireneelpro@yahoo.com>
2022-01-22 19:34:28 -0500

    js tutuoriols

---
+++ app.js
@@ -0,0 +1,167 @@
+document.write("hello world");
+// alert("hello people");
+// console.log("hello world");
+// let x = " hiren b patel";
+// console.log(x);
+// const name = "Hiren";
+// const middlename = "B";
+// const lastname = "patel";
+// console.log("My name is" + " " + name + " " + middlename + " " + lastname);
+// const fullname = name + " " + middlename + " " + lastname;
+// console.log(fullname);
+
+// let no1 = '24';
+// let no2 = '20'
+// let total = no1 + no2;
+// console.log(total);
+// let no1 = 24;
+// let no2 = 20;
+// let add = no1 + no2;
+// let sub = no1 - no2;
+// let mult = no1 * no2;
+// let div = no1 / no2;
+// let leftover = no1 % no2;
+// console.log("addition=" + add);
+// console.log("subtraction=" + sub);
+// console.log("multiplication=" + mult);
+// console.log("division=" + div);
+// console.log("remaining=" + leftover);
+
+// let x = 2;
+// let text = "hello";
+// let decide = false;
+// console.log(typeof text);
+// console.log(typeof "hello");
+// console.log(typeof false);
+// console.log(typeof x)
+// console.log(typeof 2)
+// console.log(typeof "world")
+
+// let result = 100;
+// let family = ['hiren', 'avni', 'karmit', 49, 47, 24, result]
+// console.log(family[2])
+// console.log(family[6]
+// )
+// console.log(family[4] = family[4] + 200);
+// family[4] = family[4] + 100;
+// console.log(family[4])
+
+// function hello() {
+//     console.log('hiren')
+//     console.log('avni')
+//     console.log('karmit')
+// }
+
+// hello();
+// // codelines 100
+// hello()
+
+// function student(name, marks, rank) {
+//     console.log(name + marks + rank)
+// }
+// student('hiren', 98, 'second');
+// student('hiren', 39, 1)
+// student("hirem" ,'#*$',1)
+
+// ===========function to transfer inches into centimeter.==========
+
+// function calculate(value) {
+//     let newvalue = 2.54 * value;
+
+//     return(newvalue)
+
+// }
+// let width = 25;
+// let height = 10;
+// let length = 10;
+// let cm_width = calculate(width);
+// let cm_height = calculate(height);
+// let cm_length = calculate(length);
+// let dimention = [width, height, length]
+// console.log ('dimention in inches ' + dimention)
+// let finaldimention = [cm_width, cm_height, cm_length];
+// console.log('Dimention in centimeters  ' + finaldimention)
+
+// function add(num1, num2) {
+//     return num1 + num2
+// }
+
+// const sum1 = add(5, 5);
+// const sum2 = add(10, 10);
+// const sum3 = add(15, 15);
+// let sum = [sum1,sum2,sum3]
+// console.log(sum)
+
+// const mycar = {
+//     name: 'ford',
+//     model: 250,
+//     airbag: true,
+//     color:'white',
+//     service: ['3month', '6month', '9month'],
+//     speed: function () {
+//         console.log('hello zooooom');
+//     },
+
+// }
+// console.log(mycar.color);
+// console.log(mycar.model)
+// console.log(mycar.service[2])
+// mycar.speed();
+
+// const num1 = 100;
+// const num2 = 10;
+// const result = num1 - num2;
+// console.log(typeof result)
+// if (result>0) {
+//     console.log('number 1 greater than number2')
+// }
+
+// else if(result<0) {
+//     console.log('number 2 greater than number 1')
+// }
+// else{console.log('number1 and number2 are equal')}
+
+// const num1 = 100;
+// const num2 = 10;
+// const result = num1 <= num2;
+// console.log(typeof result)
+// if (!result) {
+//     console.log('number 1 greater than number2')
+// }
+
+// else if(num2>num1) {
+//     console.log('number 2 greater than number 1')
+// }
+// else { console.log('number1 and number2 are equal') }
+
+// const num1 = 100;
+// const num2 = 10;
+// const result = num1 >num2;
+// console.log(typeof result)
+// if (result) {
+//     console.log('number 1 greater than number2')
+// }
+
+// else if(num2>num1) {
+//     console.log('number 2 greater than number 1')
+// }
+// else{console.log('number1 and number2 are equal')}
+
+// let name = 'hiren';
+// let age = 4;
+// if (name === 'hiren' && age === 49) {
+//     console.log('hello user')
+
+// }
+// else {
+//     console.log('wrong user')
+
+// }
+
+// let pocket = 8;
+// do {
+//   console.log("i have " + pocket + " dollars");
+//   pocket--;
+// } while (pocket > 1);
+
+
---
+++ app1.js
@@ -0,0 +1,106 @@
+
+// Properties and methods of string
+
+// let name = "  hiren b patel";
+// console.log(name);
+// console.log(name.length);
+// console.log(name.trim());
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.charAt(2));
+// console.log(name.charAt(11));
+// console.log(name.trim().startsWith('hiren'));
+// console.log(name.trim().slice(0,5).charAt(0).toUpperCase());
+// // console.log(name.trim().charAt(name.length - 3));
+
+// ES6- and Template literals, Backtic, interpolation
+
+// let name = 'hiren';
+// let age = 49;
+// let statement = `My name is ${name} and I'm ${age} year old, my eligible is ${49 + 5}`;
+// console.log(statement);
+
+
+// properties and methods of arrays
+
+// let names = ['hp', 'kp', 'ap,', 'up', 'bp', 'jp'];
+// const surname = ['patel', 'shah']
+// console.log(names.length);
+// console.log(names[2]);
+// console.log(names[names.length - 1]);
+
+// console.log(names.reverse());
+// let rnames = names.reverse();
+// console.log(rnames);
+
+// console.log(names.concat(surname));
+// let fullname = names.concat(surname);
+// console.log(fullname);
+
+// fullname.unshift('karmit');
+// console.log(fullname)
+
+// fullname.shift();
+// fullname.shift();
+// fullname.shift();
+// console.log(fullname);
+
+// fullname.push('hiren');
+// console.log(fullname);
+// fullname.push('kaushal')
+// console.log(fullname)
+// fullname.pop();
+// console.log(fullname);
+
+// fullname.splice(1, 1);
+// console.log(fullname)
+
+// Exercise=== to generate fullname ===========
+// const name = ['hiren', 'avni', 'karmit'];
+// console.log(name );
+// const surname = ['patel']
+// console.log(surname);
+// let fullname = [];
+// for (let i = 0; i < name.length; i++){
+//     fullname.push(`${name[i]} ${surname[0]} `)
+//     // console.log(fullname)
+
+// }
+
+// console.log(fullname);
+
+
+
+// ===========to do total of arrary===========
+let gas = [50, 30, 30];
+let food = [20, 30,25, 35, 45];
+let total
+function subtotal(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++)
+    {total=total + arr[i]
+        
+    }
+    return(total)
+}
+
+const gastotal = subtotal(gas);
+console.log(`Total gas= ${gastotal}`);
+
+const foodtotal = subtotal(food);
+console.log(`Total food= ${foodtotal}`);
+const alltotal = gastotal + foodtotal;
+console.log(`Total of all expence ${alltotal}`)
+if (alltotal >= 300) {
+    console.log('you are spending too much')
+}
+else {
+    console.log('you are ok for more spending')
+}
+
+
+
+
+
+
+
